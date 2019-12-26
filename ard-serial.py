b10a88ac91d6491d14e5b9dd2d4e87b356568581
Michael Charo <scharo@kpmg.com>
2019-12-26 11:13:16 -0600

    Survive serial disconnects Use try/except to prevent dying whenever there was a connectivity issue

--- ard-serial.py
+++ ard-serial.py
@@ -10,16 +10,19 @@ rate = 9600
 s1 = serial.Serial(port, rate)
 s1.flushInput()
 while True:
-    r = subprocess.run(["ansible", "-i", "~/hosts", "-a", "vcgencmd measure_temp", "pi"], capture_output=True, text=True)
-    r_items = [ it.strip() for it in re.split(r'\||\n', r.stdout.strip()) ]
-    ips = r_items[0::4]
-    temps = [ re.split(r'=|\.', it)[1] for it in r_items[3::4] ]
-    tempdict = dict(zip(ips, temps))
-    temps = { key:tempdict[key] for key in sorted(tempdict.keys()) }
+    try:
+        r = subprocess.run(["ansible", "-i", "~/hosts", "-a", "vcgencmd measure_temp", "pi"], capture_output=True, text=True)
+        r_items = [ it.strip() for it in re.split(r'\||\n', r.stdout.strip()) ]
+        ips = r_items[0::4]
+        temps = [ re.split(r'=|\.', it)[1] for it in r_items[3::4] ]
+        tempdict = dict(zip(ips, temps))
+        temps = { key:tempdict[key] for key in sorted(tempdict.keys()) }
 
-    print(temps)
-    message = 'A' + ','.join(temps.values()) + 'B'
+        print(temps)
+        message = 'A' + ','.join(temps.values()) + 'B'
 
-    s1.write(message.encode("ascii", "ignore"))
+        s1.write(message.encode("ascii", "ignore"))
+    except:
+        pass
 
-    time.sleep(2)
+    time.sleep(10)
